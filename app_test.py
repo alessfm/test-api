943c1ad2ffdec492d89ad3767bf5b4222649404f
alessfm <alessandrofm@aluno.fapce.edu.br>
2025-06-26 16:41:36 -0300

    Revisar testes

--- app_test.py
+++ app_test.py
@@ -1,25 +1,31 @@
-import requests
 import pytest
+import requests
 
-uuid = "acde070d-8c4c-4f0d-9d8a-162843c10333"
+
+UUID = "acde070d-8c4c-4f0d-9d8a-162843c10333"
 
 @pytest.fixture
 def base_url():
   return "http://localhost:5000"
 
+
 # Testes - Receber Transação
+def teste_salvar_campos_invalidos(base_url):
+  response = requests.post(f"{base_url}/transacao", json={})
+  assert response.status_code == 400
+
 def teste_salvar_json_invalido(base_url):
   payload = {
-    "id": uuid,
-    "v": 123.45,
-    "data": "2020-08-07T12:34:56.789-03:00"
+    "id": UUID,
+    "valor": "123.45",
+    "dataHora": "2020-08-07T12:34:56.789-03:00"
   }
   response = requests.post(f"{base_url}/transacao", json=payload)
   assert response.status_code == 400
 
 def teste_salvar_valor_invalido(base_url):
   payload = {
-    "id": uuid,
+    "id": UUID,
     "valor": -1,
     "dataHora": "2020-08-07T12:34:56.789-03:00"
   }
@@ -28,7 +34,7 @@ def teste_salvar_valor_invalido(base_url):
 
 def teste_salvar_data_invalida(base_url):
   payload = {
-    "id": uuid,
+    "id": UUID,
     "valor": 123.45,
     "dataHora": "2100-01-01T00:00:00.000-03:00"
   }
@@ -37,42 +43,43 @@ def teste_salvar_data_invalida(base_url):
 
 def test_salvar_sucesso(base_url):
   payload = {
-    "id": uuid,
+    "id": UUID,
     "valor": 123.45,
-    # "dataHora": "2020-08-07T12:34:56.789-03:00"
+    "dataHora": "2020-08-07T12:34:56.789-03:00"
   }
   response = requests.post(f"{base_url}/transacao", json=payload)
   assert response.status_code == 201
 
-def test_salvar_id_invalido(base_url):
+def test_salvar_id_repetido(base_url):
   payload = {
-    "id": uuid,
+    "id": UUID,
     "valor": 123.45,
-    "dataHora": "2100-01-01T00:00:00.000-03:00"
+    "dataHora": "2020-08-07T12:34:56.789-03:00"
   }
   response = requests.post(f"{base_url}/transacao", json=payload)
   assert response.status_code == 422
 
 
-# Testes - Recuperar uma transação
-def test_buscar_por_uuid_erro(base_url):
+# Testes - Recuperar uma Transação
+def test_buscar_por_uuid_404(base_url):
   response = requests.get(f"{base_url}/transacao/abc123")
   assert response.status_code == 404
 
-def test_buscar_por_uuid_sucesso(base_url):
-  response = requests.get(f"{base_url}/transacao/{uuid}")
-  assert response.status_code == 200
-  assert response.json()["conteudo"]["uuid"] == uuid
+def test_buscar_por_uuid_200(base_url):
+  response = requests.get(f"{base_url}/transacao/{UUID}")
+  transacao = response.json()["conteudo"]
+  assert response.status_code == 200 and transacao["uuid"] == UUID
 
 
-# Testes - Limpar Transação
+# Testes - Limpar uma Transação
 def test_deletar_por_uuid_erro(base_url):
   response = requests.delete(f"{base_url}/transacao/abc123")
   assert response.status_code == 404
 
 def test_deletar_por_uuid_sucesso(base_url):
-  response = requests.delete(f"{base_url}/transacao/{uuid}")
-  assert response.status_code == 200
+  response_delete = requests.delete(f"{base_url}/transacao/{UUID}")
+  response_get = requests.get(f"{base_url}/transacao/{UUID}")
+  assert response_delete.status_code == 200 and response_get.status_code == 404
 
 
 # Testes - Limpar Transações
